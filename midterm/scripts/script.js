162c1595fd2d565dc46e6164bf04c22b8bbb2fb7
Jeremy Sparkman <jeremyjames411@gmail.com>
2017-07-13 11:38:28 -0400

    Added deselect button functionality

--- midterm/scripts/script.js
+++ midterm/scripts/script.js
@@ -1,8 +1,25 @@
 $(function (){
+
+  var reservedSeats = [];
+
+  $(".col-lg-2").each(function(index) {
+    $(this).attr("id", "seat" + index);
+  });
+
   $("#reserveButton").on("click", function() {
     $("body").append("<div class = \"reservePopup\"> </div>");
   });
 
+  $('.col-lg-2').on('click', function (){
+    $(this).toggleClass('selected');
+
+  });
+
+  $("#deselectButton").on("click", function() {
+    $(".col-lg-2").each(function() {
+      $(this).removeClass("selected");
+    });
+  });
 
 
 });
